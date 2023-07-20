4998a9ec0744872c7420339e9b72d653e8424c06
Almaz Balbaev <kujitengu@gmail.com>
2023-07-20 17:43:07 +0600

    Changed store.ts to work without persistor

--- front/src/app/store.ts
+++ front/src/app/store.ts
@@ -1,23 +1,13 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import { persistStore } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from 'redux-persist/es/constants';
+import {currenciesReducer} from "../features/currencies/currenciesSlice";
 
 const rootReducer = combineReducers({
-  // currencies: currenciesReducer,
+  currencies: currenciesReducer,
 });
 
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-
-export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
