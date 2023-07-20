d1b173e8906b8722693b35bf3e03b621ddf87b5b
Almaz Balbaev <kujitengu@gmail.com>
2023-07-20 19:50:27 +0600

    changed directory of pages

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css'
 import {Route, Routes} from "react-router-dom";
-import Home from "./app/pages/Home";
-import Latest from "./app/pages/Latest";
+import Home from "./pages/Home";
+import Latest from "./pages/Latest";
 
 function App() {
   return (
---
+++ front/src/pages/Latest.tsx
@@ -0,0 +1,62 @@
+import React, {useEffect, useState} from 'react';
+import {
+  FormControl,
+  FormHelperText, Grid,
+  InputLabel,
+  List,
+  ListItem,
+  MenuItem,
+  Select,
+  SelectChangeEvent
+} from "@mui/material";
+import {useAppDispatch, useAppSelector} from "../app/hooks";
+import {selectCurrencies, selectExchangeRates} from "../features/currencies/currenciesSlice";
+import {fetchAll, fetchExchangeRates} from "../features/currencies/currenciesThunks";
+import dayjs from 'dayjs'
+
+const Latest = () => {
+  const dispatch = useAppDispatch();
+  const currencies = useAppSelector(selectCurrencies);
+  const rates = useAppSelector(selectExchangeRates);
+  const [baseCurrency, setBaseCurrency] = useState<string>('USD');
+  const [today, setToday] = useState<Date>(new Date());
+
+  useEffect(() => {
+    dispatch(fetchAll());
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(fetchExchangeRates(baseCurrency));
+  }, [dispatch, baseCurrency]);
+
+  const handleChange = async (event: SelectChangeEvent) => {
+    setBaseCurrency(event.target.value);
+    setToday(new Date());
+  };
+
+  return (
+    <Grid container flexDirection={"column"} alignItems={"center"} sx={{ mt: 5 }}>
+      <FormControl sx={{m: 1, minWidth: 120}}>
+        <InputLabel id="demo-simple-select-helper-label">Choose currency</InputLabel>
+        <Select
+          labelId="demo-simple-select-helper-label"
+          id="demo-simple-select-helper"
+          value={baseCurrency}
+          label="Choose currency"
+          onChange={handleChange}
+          sx={{minWidth: "200px"}}
+        >
+          {currencies ? currencies.map((el) => (
+            <MenuItem key={el.code} value={el.code}>{el.code}: {el.name_plural}</MenuItem>
+          )) : null}
+        </Select>
+        <FormHelperText>actual rates for {dayjs(today).format('D/M/YYYY')}</FormHelperText>
+      </FormControl>
+      <List>
+        {rates ? rates.map((el) => <ListItem key={el.code}>{el.code} - {el.value.toFixed(2)}</ListItem>) : null}
+      </List>
+    </Grid>
+  );
+};
+
+export default Latest;
